5f9980ca3abc19a60cfdf8aaf9349bca2b6e1ca9
mimjii <taylor7418@naver.com>
2023-05-30 01:00:09 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,125 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Head } from "next/document";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <section className="main-container">
+        <div className="nav-container">
+          <div className="nav-container-left">
+            <img className="logo" src="/logo.svg" alt="Karade" />
+          </div>
+          <div className="nav-container-right">
+            <img className="search-icon" src="/search-icon.svg" alt="search" />
+            <img className="menu-icon" src="/menu-icon.svg" alt="menu" />
+          </div>
         </div>
-      </div>
+        <div className="nav2">
+          <select className="price">
+            <option value="default">Price ($)</option>
+          </select>
+        </div>
+        <div className="main-image-container">
+          <img
+            className="main-imagebox"
+            src="https://pbs.twimg.com/media/FRxIn9BX0AI4myb?format=jpg&name=900x900"
+            alt="main-image"
+          />
+        </div>
+        <div className="cardbox">
+          <div className="cardbox-container">
+            <img className="cardbox-container-photo" />
+            <div className="cardbox-container-writer"></div>
+          </div>
+          <div className="line"></div>
+          <div className="cardbox-info">
+            <div className="cardbox-info-title"></div>
+            <div className="cardbox-info-detail"></div>
+          </div>
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <style jsx>{`
+        .main-container {
+          height: 100vh;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          max-width: 420px;
+          margin: 0 auto;
+          position: relative;
+          border: 1px solid black;
+        }
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        .nav-container {
+          position: absolute;
+          width: 100%;
+          height: 29px;
+          top: 57px;
+          display: flex;
+          justify-content: space-between;
+        }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        .nav-container-left {
+        }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        .nav-container-right {
+          margin-right: 25px;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          padding: 5px;
+          display: flex;
+        }
+        .logo {
+          margin-left: 25px;
+        }
+
+        .search-icon {
+          height: 20px;
+          margin-right: 25px;
+          width: 20px;
+          border-radius: 0px;
+        }
+        .menu-icon {
+          margin-top: 2px;
+          height: 16px;
+          width: 17px;
+        }
+        .nav2 {
+          font-family: "Neue Haas Unica";
+          font-style: normal;
+          font-weight: 400;
+          font-size: 13px;
+          line-height: 13px;
+          color: #1a1a1a;
+          position: absolute;
+          top: 102px;
+          width: 100%;
+          height: 50px;
+          border: 1px solid #d8d8d8;
+          display: flex;
+          align-items: center;
+          justify-content: right;
+        }
+
+        .price {
+          position: relative;
+          right: 15px;
+        }
+
+        .main-image-container {
+          position: absolute;
+          top: 152px;
+          display: flex;
+          height: 470px;
+          width: 100%;
+          border: 1px solid #d8d8d8;
+          align-items: center;
+          justify-content: center;
+        }
+        .main-imagebox {
+          height: 400px;
+          width: 325px;
+        }
+      `}</style>
+    </>
+  );
 }
